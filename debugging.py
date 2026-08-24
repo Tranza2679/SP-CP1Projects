dd41e2790ebed485a7d005be491b8f66dbcee4b8
Tranza2679 <santiago.pineda@ucas-edu.net>
2026-08-24 09:25:09 -0600

    Finished my fix the code assignment

---
+++ debugging.py
@@ -0,0 +1,8 @@
+# Santiago Pineda, Fix the Code
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+#Fixed: Name had been nam in the print statement, so it wouldn't be printed
+#Fixed: The print statement had been missing a closing parenthesis 
