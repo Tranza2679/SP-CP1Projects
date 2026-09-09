9964d3d1a31ae40c8e6f9be516f994c02f6a121a
Tranza2679 <santiago.pineda@ucas-edu.net>
2026-09-09 17:51:25 +0000

    Almost finished idiot proof

--- idiot_proof.py
+++ idiot_proof.py
@@ -23,7 +23,16 @@ last_fixed = "".join(last_seperated)
 full_name = first_fixed.title() + " " + last_fixed.title()
 print("Hello " +full_name.title())
 
-string_phone_num = str(phone_num).split()
+string_phone_num = str(phone_num)
 first_part = string_phone_num[0:3]
+second_part = string_phone_num[4:7]
+third_part = string_phone_num[7:13]
+full_phone_num = first_part + " " + second_part + " " + third_part
+
 print(first_part)
+print(second_part)
+print(third_part)
+print(full_phone_num)
+
+
 
