bed3f3f735a11b2051a24f530a4e7000b40aa56a
Tranza2679 <santiago.pineda@ucas-edu.net>
2026-09-10 17:50:01 +0000

    Made my roller so much simpler

--- dice_roller.py
+++ dice_roller.py
@@ -2,36 +2,14 @@
 import random
 print("Hello, welcome to dice roller")
 print("The options of dice you have include: D4, D6, D8, D10, D12, D20")
-dice = 1
 while True:
     try:
-        dice = int(input("Please write how much sides the dice you want has: "))
-        if dice % 2 == 1:
-            print("Please note your dice should be even sided!")
-            choice = "bad"
-            while choice == "bad":
-                try: 
-                    dice = int(input("Please write how much sides the dice you want has: "))
-                except:
-                    print("Please just write it as an even sided dice")
-                else:
-                    choice = "good"
-        elif dice > 20:
-            print("That's way too big of a dice for this!")
-            choice = "bad"
-            while choice == "bad":
-                try: 
-                    dice = int(input("Please write how much sides the dice you want has: "))
-                except:
-                    print("Please just write it less than 20")
-                else:
-                    choice = "good"
-        else:
-            print("I love your choice!")
+        dice = int(input("Please write how much sides the dice you want has as a whole number: "))
     except:
-       print("Please write an actual dice!!!!")
+        print("Please write it as a whole number.")
     else:
         break
+
 
 dice_roll = random.randint(1, dice)
 print(f"You got {dice_roll} from your D{dice}")
