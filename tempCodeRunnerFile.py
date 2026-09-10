3f7be4d8e912417ce081dac717e9e36b479e9711
Tranza2679 <santiago.pineda@ucas-edu.net>
2026-09-10 09:52:25 -0600

    Worked on my dice roller

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+if dice % 2 == 1:
+            print("Please note your dice should be even sided!")
+        elif dice > 20:
+            print("That's way too big of a dice for this!")
+        else:
+            print("alright")
