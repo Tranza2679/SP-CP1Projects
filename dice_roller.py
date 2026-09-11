1bc7bc4b7865cfe24af615478a61067a273fe182
Tranza2679 <santiago.pineda@ucas-edu.net>
2026-09-11 09:39:33 -0600

    Finished my dice roller

--- dice_roller.py
+++ dice_roller.py
@@ -9,7 +9,24 @@ while True:
         print("Please write it as a whole number.")
     else:
         break
-
-
+while dice % 2 == 1:
+    print("A dice really can't be odd you know? It's an odd thing to write!!!!")
+    while True:
+        try:
+            dice = int(input("Please write how much sides the dice you want has as a whole number: "))
+        except:
+            print("Please write it as a whole number.")
+        else:
+            break
+while dice > 20:
+    print("That dice is ummm really big. Maybe write it as one of the options...?")
+    while True:
+        try:
+            dice = int(input("Please write how much sides the dice you want has as a whole number: "))
+        except:
+            print("Please write it as a whole number.")
+        else:
+            break
+print(f"Great option for a dice! I probably would've picked the same thing as {dice} is my favorite number!")
 dice_roll = random.randint(1, dice)
-print(f"You got {dice_roll} from your D{dice}")
+print(f"You got a {dice_roll} from your D{dice}!")
